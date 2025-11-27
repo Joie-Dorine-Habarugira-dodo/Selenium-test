16b00632f22a4fef0f9afe838499fe2b4f0b24cc
Joie-Dorine-Habarugira-dodo <joiedorine.habarugira22a@kepler.org>
2025-11-27 20:37:23 +0200

    Setting up the project

---
+++ Selenium-Test/src/main/java/websites/website1/HomePage.java
@@ -0,0 +1,13 @@
+package websites.website1;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class HomePage {
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
+
