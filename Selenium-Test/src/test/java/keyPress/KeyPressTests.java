3db9722762bd40e51718afb71628cd4cc4f3a406
Joie-Dorine-Habarugira-dodo <joiedorine.habarugira22a@kepler.org>
2025-11-28 14:59:58 +0200

    progress

--- Selenium-Test/src/test/java/keyPress/KeyPressTests.java
+++ Selenium-Test/src/test/java/keyPress/KeyPressTests.java
@@ -12,7 +12,7 @@ public class KeyPressTests extends BaseTests {
         HomePage homePage = new HomePage(driver);
         homePage.clickKeyPress();
         KeyPressPage keyPressPage = new KeyPressPage(driver);
-        String text="jane"
+        String text="jane";
         keyPressPage.inputText(text);
         assertEquals(keyPressPage.getText(),"You entered: jane", "incorrect text");
     }
